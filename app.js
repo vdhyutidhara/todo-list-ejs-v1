395b446f72c11d0524f55ba4c8db1987932a1feb
Vinay Gupta <Vinay.dhyutidhara@gmail.com>
2022-01-27 14:35:24 +0530

    basic day displaying

--- app.js
+++ app.js
@@ -3,16 +3,49 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
     var today = new Date();
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var currentDay = today.getDay();
+    var day = "";
 
     if (currentDay === 6 || currentDay === 0) {
-        res.send("Yay! it's weekend");
+        day = weekday[currentDay];
     } else {
-        res.sendFile(__dirname + "/index.html");
+        day = weekday[currentDay];
     }
+
+    // switch (currentDay) {
+    //     case 0:
+    //         day = "Sunday";
+    //         break;
+    //     case 1:
+    //         day = "Monday";
+    //         break;
+    //     case 2:
+    //         day = "Tuesday";
+    //         break;
+    //     case 3:
+    //         day = "Wednesday";
+    //         break;
+    //     case 4:
+    //         day = "Thursday";
+    //         break;
+    //     case 5:
+    //         day = "Friday";
+    //         break;
+    //     case 6:
+    //         day = "Saturday";
+    //         break;
+    //     default:
+    //         console.log("Error: cuurent day is " + currentDay);
+    // }
+
+    res.render("list", {
+        kindOfDay: day
+    });
 });
 
 app.listen(3000, () => {
